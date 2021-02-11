7472eed4563285c7b1c8352ffd0e7c7bb434bf8a
ngurealestate-itadmin <itadmin@ngurealestate.com.au>
2021-02-11 13:43:44 +1000

    added meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 export default function Home() {
   return (
     <div class="page-container">
+      <Helmet>
+        <title>Zync Media</title>
+        <meta name="title" content="Zync Media" />
+        <meta name="description" content="Here at NGU Media in Brisbane, our team has been producing high quality content for brands across Brisbane for the last 3 years and would love for you to be a part of our journey. " />
+      </Helmet>
       <div class="video-wrapper">
         <div class="video-text-container"></div>
         <video muted autoplay="autoplay" loop="true" width="100%">
